56ac2510a716ff2939eb2a9da75756f2986a2e73
pnshad <pnshad@gmail.com>
2023-03-31 23:51:43 +0200

    updates

--- libft/ck_ft_bzero.c
+++ libft/ck_ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 21:48:55 by pnourish          #+#    #+#             */
-/*   Updated: 2023/03/31 22:12:37 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/03/31 23:42:10 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 int main(void)
 {
-	char *test1;
-	
-	test1 = "replace me with zeroes if you can!";
+	char *test1 = "replace me with zeroes if you can!";
 	ft_bzero(test1, 10);
 	printf("the out: %s", test1);
 	return (0);
