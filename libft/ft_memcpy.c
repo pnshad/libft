add0b777f5f1d2341c2de81bd1531677c7aa8b09
pnshad <pnshad@gmail.com>
2023-04-05 02:52:46 +0200

    after norminette memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 01:15:44 by pnourish          #+#    #+#             */
-/*   Updated: 2023/04/05 02:46:20 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/04/05 02:52:02 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	while (n > 0)
 	{
-		((unsigned char *)dst)[n - 1 ] = ((unsigned char *)src)[n - 1];
+		((unsigned char *)dst)[n - 1] = ((unsigned char *)src)[n - 1];
 		n--;
 	}
 	return (dst);
