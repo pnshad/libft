53f4f5af3acd4e051900496aee38875074b29882
Pezhman Nourishad <pnourish@e4r1p4.42roma.it>
2023-04-07 17:59:14 +0200

    after norminette strnstr()

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 20:53:07 by pnourish          #+#    #+#             */
-/*   Updated: 2023/04/07 16:26:05 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/04/07 17:57:59 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	litlen = ft_strlen(little);
 	if (litlen > len || len == 0)
-		return NULL;
+		return (NULL);
 	if (litlen == 0)
 		return ((char *)big);
-	while (*big &&  len >= litlen)
+	while (*big && len >= litlen)
 	{
 		if (ft_memcmp(big, little, litlen) == 0)
-			return((char *)big);
+			return ((char *)big);
 		big++;
 		len--;
 	}
