7a89708bae1b08bd5b70e9e852db147966579153
pnshad <pnshad@gmail.com>
2023-04-08 19:40:46 +0200

    after success in strlcpy ft and ck both

--- libft/ck_ft_strlcpy.c
+++ libft/ck_ft_strlcpy.c
@@ -6,63 +6,63 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 20:01:59 by pnourish          #+#    #+#             */
-/*   Updated: 2023/04/08 00:04:38 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/04/08 19:13:03 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 int main(void)
 {
 	char dst5a[5];
-	char dst0a[1];
+	char dst0a[] = "";
 	char dst1a[1] = {'1'};
 	char dst5b[5];
-	char dst0b[1];
+	char dst0b[] = "";
 	char dst1b[1] = {'1'};
 	char src2[] = "Hi";
 	char src5[] = "Hello";
 	char src9[] = "Hello, Hi";
-	char srcn[] = "H\0i,\n";
+	char srcn[] = "Hi,nn";
 	size_t d5s2 = 4; 
 	size_t d5s5 = 5;
 	size_t d5s9 = 4;
-	size_t d0s2 = 1;
+	size_t d0s2 = 0;
 	size_t d1s2 = 1;
 	size_t d5sn = 4;
 
 	ft_strlcpy(dst5a, src2, d5s2);
 	strlcpy(dst5b, src2, d5s2);
-	printf("ft_strlcpy:\t%s\n", dst5a);
-	printf("sy_strlcpy:\t%s\n", dst5b);
+	printf("d5s2_ft_strlcpy:\t%s\n", dst5a);
+	printf("d5s2_sy_strlcpy:\t%s\n", dst5b);
 	assert(strcmp(dst5a, dst5b) == 0);
 
 	ft_strlcpy(dst5a, src5, d5s5);
 	strlcpy(dst5b, src5, d5s5);
-	printf("ft_strlcpy:\t%s\n", dst5a);
-	printf("sy_strlcpy:\t%s\n", dst5b);
+	printf("d5s5_ft_strlcpy:\t%s\n", dst5a);
+	printf("d5s5_sy_strlcpy:\t%s\n", dst5b);
 	assert(strcmp(dst5a, dst5b) == 0);
 
 	ft_strlcpy(dst5a, src9, d5s9);
 	strlcpy(dst5b, src9, d5s9);
-	printf("ft_strlcpy:\t%s\n", dst5a);
-	printf("sy_strlcpy:\t%s\n", dst5b);
+	printf("d5s9_ft_strlcpy:\t%s\n", dst5a);
+	printf("d5s9_sy_strlcpy:\t%s\n", dst5b);
 	assert(strcmp(dst5a, dst5b) == 0);
 
 	ft_strlcpy(dst0a, src2, d0s2);
 	strlcpy(dst0b, src2, d0s2);
-	printf("ft_strlcpy:\t%s\n", dst0a);
-	printf("sy_strlcpy:\t%s\n", dst0b);
+	printf("d0s2_ft_strlcpy:\t%s\n", dst0a);
+	printf("d0s2_sy_strlcpy:\t%s\n", dst0b);
 	assert(strcmp(dst0a, dst0b) == 0);
 
 	ft_strlcpy(dst1a, src2, d1s2);
 	strlcpy(dst1b, src2, d1s2);
-	printf("ft_strlcpy:\t%s\n", dst1a);
-	printf("sy_strlcpy:\t%s\n", dst1b);
+	printf("d1s2_ft_strlcpy:\t%s\n", dst1a);
+	printf("d1s2_sy_strlcpy:\t%s\n", dst1b);
 	assert(strcmp(dst1a, dst1b) == 0);
 
-	ft_strlcpy(dst5a, srcn, d5sn);
+	strlcpy(dst5a, srcn, d5sn);
 	strlcpy(dst5b, srcn, d5sn);
-	printf("ft_strlcpy:\t%s\n", dst5a);
-	printf("sy_strlcpy:\t%s\n", dst5b);
+	printf("d5sn_ft_strlcpy:\t%s\n", dst5a);
+	printf("d5sn_sy_strlcpy:\t%s\n", dst5b);
 	assert(strcmp(dst5a, dst5b) == 0);
 
 	printf("the function passed all test cases!\n");
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 18:01:01 by pnourish          #+#    #+#             */
-/*   Updated: 2023/04/07 20:58:51 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/04/08 19:18:21 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t srclen;
+	size_t	srclen;
 
-	srclen = ft_strlen(src);
-	if (srclen >= size)
-		size = size - 1;
-	else
-		size = srclen;
-	ft_memcpy(dst, src, size);
-	dst[size] = '\0';
+	srclen = (size_t)ft_strlen(src);
+	if (!(*dst == '\0' || size == 0))
+	{
+		if (srclen >= size)
+			size = size - 1;
+		else
+			size = srclen;
+		ft_memcpy(dst, src, size);
+		dst[size] = '\0';
+	}
 	return (srclen);
 }
