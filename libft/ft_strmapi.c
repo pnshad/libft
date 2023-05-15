acfa453c15f811ddbce9af097f4e90dc322341ec
pnshad <pnshad@gmail.com>
2023-05-15 19:09:42 +0200

    ft_astrmapi after norminette

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,25 +6,30 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 04:54:22 by pnourish          #+#    #+#             */
-/*   Updated: 2023/05/15 15:23:28 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/05/15 19:08:46 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    size_t  len;
-    char    *result;
+	size_t	len;
+	char	*result;
+	size_t	i;
 
-    if (!s || !f)
-        return (NULL);
-    len = ft_strlen(s);
-    result = malloc(sizeof(char) * (len + 1));
-    if (!result)
-        return (NULL);
-    result[len] = '\0';
-    for (unsigned int i = 0; i < len; i++)
-        result[i] = f(i, s[i]);
-    return (result);
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	while (i < len)
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	return (result);
 }
