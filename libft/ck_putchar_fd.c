05fed92b7d33c408cfe7426407204e85d1c6ca6e
pnshad <pnshad@gmail.com>
2023-05-16 20:37:54 +0200

    refactored the ck_putchar_fd

--- libft/ck_putchar_fd.c
+++ libft/ck_putchar_fd.c
@@ -6,34 +6,85 @@
 /*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 01:53:23 by pnourish          #+#    #+#             */
-/*   Updated: 2023/05/16 02:13:47 by pnourish         ###   ########.fr       */
+/*   Updated: 2023/05/16 20:21:05 by pnourish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdio.h>
-#include <fcntl.h>
+void	ft_putchar_fd(char c, int fd);
 
-void ft_putchar_fd(char c, int fd);
+void    run_putchar_fd_test_case(char *s,char *d);
 
-void	ck_putchar_fd(void)
+void ck_putchar_fd(void)
 {
-    int fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644); // open file for writing
+    printf("\nft_putchar_fd >>> testing...\n\n");
+
+    run_putchar_fd_test_case("Hello, World!", "Basic test to see if ft_putchar_fd writes successfully inside a file");
+    run_putchar_fd_test_case("1234567890", "Test with numbers");
+    run_putchar_fd_test_case("Testing\ttab", "Test with tab character");
+    run_putchar_fd_test_case("New\nline", "Test with newline character");
+    run_putchar_fd_test_case("", "Test with empty string");
+    run_putchar_fd_test_case("Special\\ \" ' \n \t", "Test with special characters");
+
+    printf("function passed all test cases successfully!\n");
+    printf("---- ---- ---- ---- ---- ---- ---- ---- ----\n");
+}
+
+void 	run_putchar_fd_test_case(char *s,char *d)
+{
+	size_t	i;
+    
+	printf("Test case %s\n", d);
+    printf("Source (expected output): ");
+    my_printa(s, 0, sizeof(char), "%c");
+	int fd = open("test_putchar_fd_file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644); // open file for writing
     if (fd == -1)
     {
-        perror("open"); // print error message if file cannot be opened
+        perror("unable to create/access the file"); // print error message if file cannot be opened
+        return;
+    }
+	i = 0;
+	while (s[i])
+	{
+    	ft_putchar_fd(s[i++], fd);
+    }
+	close(fd); // close file
+    
+	fd = open("test_putchar_fd_file.txt", O_RDONLY); // open file for reading
+    if (fd == -1)
+    {
+        perror("unable to access the file"); // print error message if file cannot be opened
         return;
     }
 
-    ft_putchar_fd('H', fd); // write character 'H' to file
-    ft_putchar_fd('e', fd); // write character 'e' to file
-    ft_putchar_fd('l', fd); // write character 'l' to file
-    ft_putchar_fd('l', fd); // write character 'l' to file
-    ft_putchar_fd('o', fd); // write character 'o' to file
+    // Measure the file's contents size
+    off_t file_size = lseek(fd, 0, SEEK_END);
+
+    // Allocate buffer dynamically based on the file's contents size
+    char* out_ft = (char*)malloc(file_size + 1);
+    if (out_ft == NULL)
+    {
+        perror("unable to allocate memory for the buffer");
+        close(fd);
+        return;
+    }
+
+    // Reset the file pointer to the beginning of the file
+    lseek(fd, 0, SEEK_SET);
+
+    // Read the file's contents into the dynamically allocated buffer
+    read(fd, out_ft, file_size);
+
+    out_ft[file_size + 1] = '\0'; // Add null-terminator at the end of the buffer
 
     close(fd); // close file
-
-    printf("File written successfully.\n");
+	printf("\n   ft_putchar_fd output): ");
+    my_printa(out_ft, 0, sizeof(char), "%c");
+    printf("\n\n");
+	assert(strcmp(out_ft, s) == 0);
+	
+    // Free the dynamically allocated buffer
+    free(out_ft);
 }
 
