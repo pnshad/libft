9cecba33803e96ea723fbfab52c5883d58f0f592
Pezhman Nourishad <pnshad@gmail.com>
2023-07-10 23:14:13 +0200

    strlen output to size_t

--- new_libft/libft.h
+++ new_libft/libft.h
@@ -34,11 +34,11 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_strlen(const char *s);
 int		ft_atoi(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
