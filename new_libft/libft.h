c9d49aed2ac694a45afd3f1c6c0058ac12cddc35
Pezhman Nourishad <pnshad@gmail.com>
2023-07-11 21:03:25 +0200

    ft_strlen_np

--- new_libft/libft.h
+++ new_libft/libft.h
@@ -39,6 +39,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strlen_np(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
